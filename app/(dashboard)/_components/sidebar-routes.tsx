147fea868919c02d67dcf0b04b8a83373ef35c58
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2024-11-10 00:11:03 +0500

    updated

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,5 +1,6 @@
 "use client"
-import {Compass, Layout} from "lucide-react";
+import { usePathname } from "next/navigation";
+import {BarChart, Compass, Layout, List} from "lucide-react";
 import { SidebarItem } from "./sidebar-item";
 const guestRoutes = [
     {
@@ -13,10 +14,26 @@ const guestRoutes = [
         href: "/search",
     },
 
+];
+const teacherRoutes = [
+    {
+        icon: List,
+        label: "Courses",
+        href: "/teacher/courses",
+    },
+    {
+        icon: BarChart,
+        label: "Analytics",
+        href: "/teacher/analytics",
+    },
+
 ]
 
 export const SidebarRoutes = () =>{
-    const routes = guestRoutes;
+    const pathname = usePathname ();
+    const isTeacherPage = pathname?.includes("/teacher");
+
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
 
     return (
